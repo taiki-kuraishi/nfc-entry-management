cad6c8a9d3796781b7bbf9101c80f814f56ee9d5
kuraishi <20122027@kaishi-pu.ac.jp>
2024-02-10 21:12:45 +0900

    Add tests for ewpository in spi-server #9

--- api/src/main.go
+++ api/src/main.go
@@ -7,10 +7,21 @@ import (
 	"api/router"
 	"api/usecase"
 	"api/validator"
+	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
 )
 
 func main() {
-	db := db.ConnectDB()
+	url := fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_DATABASE"))
+	mysqlConfig := mysql.Config{
+		DriverName:                "mysql",
+		DSN:                       url,
+		SkipInitializeWithVersion: true,
+	}
+
+	db := db.ConnectDB(mysqlConfig)
 	entryValidator := validator.NewEntryValidator()
 	userValidator := validator.NewUserValidator()
 	userRepository := repository.NewUserRepository(db)
--- api/src/migrate/migrate.go
+++ api/src/migrate/migrate.go
@@ -4,10 +4,18 @@ import (
 	"api/db"
 	"api/model"
 	"fmt"
+	"os"
+
+	"gorm.io/driver/mysql"
 )
 
 func main() {
-	dbConn := db.ConnectDB()
+	mysqlConfig := mysql.Config{
+		DriverName: "mysql",
+		DSN:        fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_DATABASE")),
+	}
+
+	dbConn := db.ConnectDB(mysqlConfig)
 	defer fmt.Println("Successfully Migrated")
 	defer db.CloseDB(dbConn)
 	err := dbConn.AutoMigrate(&model.User{}, &model.Entry{})
