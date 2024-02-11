682e2c2939b4adf5bb78c59392054eee62c5eac7
kuraishi <20122027@kaishi-pu.ac.jp>
2024-02-11 18:27:46 +0900

    Add send_request_to_api function in nfc_reader #9

--- nfc_reader/src/main.py
+++ nfc_reader/src/main.py
@@ -3,9 +3,12 @@
 """
 
 import os
+import threading
+import time
 from dataclasses import dataclass
 
 import nfc
+import requests
 from dotenv import load_dotenv
 
 
@@ -56,7 +59,7 @@ def read_nfc_tag(tag: nfc.tag.Tag, config: Configuration) -> NfcTagInfo:
     student_num = tag.read_without_encryption([sc], [bc])
     if isinstance(student_num, str):
         student_num = student_num.encode("shift_jis")
-    student_num = student_num.decode("shift_jis")
+    student_num = student_num.decode("shift_jis").strip("\x00").strip("\x001")
     print("student_number : " + str(student_num))
 
     # name
@@ -64,10 +67,31 @@ def read_nfc_tag(tag: nfc.tag.Tag, config: Configuration) -> NfcTagInfo:
     name = tag.read_without_encryption([sc], [bc])
     if isinstance(name, str):
         name = name.encode("shift_jis")
-    name = name.decode("shift_jis")
+    name = name.decode("shift_jis").strip("\x00").strip("\x001")
     print("name : " + str(name))
 
     return NfcTagInfo(idm, pmm, config.nfc_system_code, student_num, name)
+
+
+def send_request_to_api(nfc_tag_info: NfcTagInfo, unix_timestamp: float) -> None:
+    """
+    NFCタグの情報とUnixタイムスタンプをAPIに送信します。
+
+    Parameters:
+    nfc_tag_info (NfcTagInfo): NFCタグの情報を含むオブジェクト
+    unix_timestamp (float): Unixタイムスタンプ（1970年1月1日からの経過秒数）
+    """
+    url = os.environ["API_URL"]
+    headers = {"Content-Type": "application/json"}
+    body = {
+        "student_number": int(nfc_tag_info.student_num),
+        "name": nfc_tag_info.name,
+        "timestamp": unix_timestamp,
+    }
+
+    response = requests.post(url, json=body, headers=headers, timeout=5)
+    print(response.status_code)
+    print(response.text)
 
 
 def on_connect(tag: nfc.tag.Tag) -> bool:
@@ -90,6 +114,12 @@ def on_connect(tag: nfc.tag.Tag) -> bool:
 
     nfc_tag_info = read_nfc_tag(tag, configuration)
     print(nfc_tag_info)
+    current_unix_time = time.time()
+
+    thread = threading.Thread(
+        target=send_request_to_api, args=(nfc_tag_info, current_unix_time)
+    )
+    thread.start()
 
     return True
 
